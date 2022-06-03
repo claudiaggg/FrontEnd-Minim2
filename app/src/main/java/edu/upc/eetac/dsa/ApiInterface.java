2b2adfd7ac057b0a62e813e6bc5860991ceee513
clauggg <claudia.garcia.gil@estudiantat.upc.edu>
2022-06-03 17:43:08 +0200

    Entrega minim 2 sobre el projecte frontend

---
+++ app/src/main/java/edu/upc/eetac/dsa/ApiInterface.java
@@ -0,0 +1,35 @@
+package edu.upc.eetac.dsa;
+import java.util.List;
+
+import edu.upc.eetac.dsa.models.Item;
+import edu.upc.eetac.dsa.models.LogInParams;
+import edu.upc.eetac.dsa.models.Stats;
+import edu.upc.eetac.dsa.models.User;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface ApiInterface {
+
+    @GET("users/catalogo")
+    Call<List<Item>> getItems();
+
+    @GET("stats/ranking")
+    Call<List<Stats>> getRanking();
+
+
+    @POST("users/login")
+    Call<User> login(@Body LogInParams loginpar);
+
+
+    @POST("users/register")
+    Call<User> register(@Body User user);
+
+    @GET("users")
+    Call<List<User>> getUsers();
+
+    @GET("catalogo")
+    Call<List<Item>> getCatalogo();
+
+    @POST("users/profile")
+    Call<User> profile (@Body String username);
+}
